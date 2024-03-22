1d241196fb18d25ad41458b542c59e12045a3d27
Parcifall-IT <83290339+Parcifall-IT@users.noreply.github.com>
2024-03-22 19:11:20 +0500

    Pause v0.1 added
    Добавил паузу в игре без функционала кнопок

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed = 35.0f;
+    private Vector2 moveVector;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        moveVector.x = Input.GetAxis("Horizontal");
+        moveVector.y = Input.GetAxis("Vertical");
+        rb.MovePosition(rb.position + moveVector * speed * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/UI/PauseCanvas.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class PauseCanvas : MonoBehaviour
+{
+    public bool isOpened = false; //Открыто ли меню
+    public int isStopped = 1; //Остановлено ли время
+    public float volume = 0; //Громкость
+    public int quality = 0; //Качество
+    public bool isFullscreen = false; //Полноэкранный режим
+    public AudioMixer audioMixer; //Регулятор громкости
+    public Dropdown resolutionDropdown; //Список с разрешениями для игры
+    private Resolution[] resolutions; //Список доступных разрешений
+    private int currResolutionIndex = 0; //Текущее разрешение
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            ShowHideMenu();
+        }
+    }
+
+    public void ShowHideMenu()
+    {
+        isStopped = (isStopped + 1) % 2;
+        Time.timeScale = isStopped;
+
+        isOpened = !isOpened;
+        GetComponent<Canvas>().enabled = isOpened;
+    }
+}
