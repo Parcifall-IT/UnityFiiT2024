351c4faf468bf52a4f6f69e6477ebf83746f6214
Iesh <108469591+Iesht@users.noreply.github.com>
2024-05-09 20:09:46 +0500

    Added 2 rooms
    Пока странно работает переключение камеры

---
+++ Assets/Scripts/Camera/CameraSwitcher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera nextCamera;
+
+    private bool isInside;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (isInside)
+            {
+                isInside = false;
+                return;
+            }
+            isInside = true;
+            nextCamera.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && isInside)
+        {
+            nextCamera.gameObject.SetActive(false);
+            isInside = false;
+        }
+    }
+}
