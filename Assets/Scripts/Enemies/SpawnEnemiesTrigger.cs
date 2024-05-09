629fbbded6003a1183eb3255884a503d46386d1d
KodoyTheFloppa <a.punenko@mail.ru>
2024-05-09 18:38:16 +0500

    Added spawn button
    Добавлена кнопка при наступании на которую появляются враги в на случайных координатах внутри комнаты

---
+++ Assets/Scripts/Enemies/SpawnEnemiesTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemiesTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject EnemyType;
+    [SerializeField] int AmountOfEnemies;
+    [SerializeField] string TagFilter;
+    [SerializeField] float minX;
+    [SerializeField] float maxX;
+    [SerializeField] float minY;
+    [SerializeField] float maxY;
+    private float xPos;
+    private float yPos;
+    private int spawnedEnemies;
+    private bool isAbleToSpawn = true;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!string.IsNullOrEmpty(TagFilter) && !collision.gameObject.CompareTag(TagFilter))
+            return;
+        if (isAbleToSpawn)
+        {
+            SpawnEnemies();
+            isAbleToSpawn = false;
+        } 
+    }
+
+    void SpawnEnemies()
+    {
+        while (spawnedEnemies < AmountOfEnemies)
+        {
+            xPos = Random.Range(minX, maxX);
+            yPos = Random.Range(minY, maxY);
+            Instantiate(EnemyType, new Vector2(xPos, yPos), Quaternion.identity);
+            spawnedEnemies++;
+        }
+    }
+}
