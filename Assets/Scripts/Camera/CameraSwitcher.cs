144190be7fb73437bddd3b0be23ba571ee5ad3a5
Iesh <108469591+Iesht@users.noreply.github.com>
2024-05-10 00:10:32 +0500

    All
    Всё собрано вместе, кажется

---
+++ Assets/Scripts/Camera/CameraSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    public CinemachineVirtualCameraBase[] virtualCameras;
+    private int currentCameraIndex;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SwitchCamera();
+        }
+    }
+
+    private void SwitchCamera()
+    {
+        foreach (var camera in virtualCameras)
+        {
+            camera.gameObject.SetActive(false);
+        }
+        currentCameraIndex = (currentCameraIndex + 1) % virtualCameras.Length;
+
+        virtualCameras[currentCameraIndex].gameObject.SetActive(true);
+    }
+}
